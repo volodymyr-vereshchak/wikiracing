9f7e041ed8eaf2df373bd04e75257d43f8240479
Volodymyr Vereshchak <diakonx1985@gmail.com>
2023-01-19 16:42:22 +0200

    main algoritm

--- settings.py
+++ settings.py
@@ -2,3 +2,4 @@ DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/wiki_db"
 REQUESTS_PER_MINUTE = 100
 LINKS_PER_PAGE = 200
 BASE_URL = "https://uk.wikipedia.org/wiki/"
+MAX_DEPTH = 6
